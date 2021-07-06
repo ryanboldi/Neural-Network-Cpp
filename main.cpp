2415f22a5ddce80ce5d8d77fb57bca8b0a8e6cd5
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-06 19:42:35 -0400

    getError implemented

--- main.cpp
+++ main.cpp
@@ -9,33 +9,55 @@
 using namespace std;
 
 int XOR(int a, int b);
+double eval(Network n);
+double getError(Network n, std::vector<double> inputs, std::vector<double> expOutputs);
 
 int main(){
-    Network n(2, 5, 2);
+    Network n(2, 1, 1);
     cout << n << endl;
 
     n.fullyConnect();
 
-    cout << n << endl;
+    cout << eval(n);
 
-    auto result = n.feedForward({0.2, 1.7});
-    std::copy(result.begin(), result.end(), std::ostream_iterator<double>(std::cout, " "));
+    //cout << n << endl;
 
-    cout << n << endl;
+    //auto result = n.feedForward({0.2, 1.7});
+    //std::copy(result.begin(), result.end(), std::ostream_iterator<double>(std::cout, " "));
 
-    n.feedForward({0.2, 1.7});
+    //cout << "XOR(0, 0) = " << XOR(0, 0);
+    //cout << "XOR(0, 1) = " << XOR(0, 1);
+    //cout << "XOR(1, 0) = " << XOR(1, 0);
+    //cout << "XOR(1, 1) = " << XOR(1, 1);
+
     
-    result = n.feedForward({0.2, 1.7});
-    std::copy(result.begin(), result.end(), std::ostream_iterator<double>(std::cout, " "));
-
-    n.feedForward({0.2, 1.7});
-
-    cout << "XOR(0, 0) = " << XOR(0, 0);
-    cout << "XOR(0, 1) = " << XOR(0, 1);
-    cout << "XOR(1, 0) = " << XOR(1, 0);
-    cout << "XOR(1, 1) = " << XOR(1, 1);
 }
 
+double eval(Network n){
+    double totalError = 0;
+    totalError+=getError(n, {0,0}, {0});
+    totalError+=getError(n, {0,1}, {1});
+    totalError+=getError(n, {1,0}, {1});
+    totalError+=getError(n, {1,1}, {0});
+
+    return totalError;
+}
+
+double getError(Network n, std::vector<double> inputs, std::vector<double> expOutputs){
+    double error = 0;
+    
+    for (int i = 0; i < 5; i++){
+        n.feedForward(inputs);
+    }
+    
+    std::vector<double> outputs = n.feedForward(inputs);
+
+    for (int i=0; i < expOutputs.size() ; i++){
+        error += outputs[i] - expOutputs[i];
+    }
+
+    return error;
+}
 
 int XOR(int a, int b){
     if (a == b) {
